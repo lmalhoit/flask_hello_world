5aa23c1aa32cc648caa058eed0863254f01ea195
Lauren Malhoit <laurenmalhoit@gmail.com>
2015-04-29 18:53:03 +0000

    add precompiled assets to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn flask_hello_world:app --log-file=-
--- hello_world.py
+++ hello_world.py
@@ -1,28 +1,16 @@
-from flask import Flask
 
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/hello")
 def hello_world():
-    return "Hello World!"
-
+    #return "Hello World!"
+    return render_template('template.html')
+  
 @app.route("/hello/<name>")
 def hello_person(name):
-    html = """
-        <h1>
-            Hello {}!
-        </h1>
-        <p>
-            Here's a picture of a kitten.  Awww...
-        </p>
-        <img src="http://placekitten.com/g/200/300">
-    """
-    return html.format(name.title())
-  
-@app.route("/hello/jedi/<name1>/<name2>")
-def hello_jedi(name1, name2):
-    return "Your Jedi name is {}".format(name1[0:3].title + name2[0:2]) 
+    return render_template('template.html')
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8888)
